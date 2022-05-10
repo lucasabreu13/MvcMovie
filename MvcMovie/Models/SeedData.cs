2ecbbe771d858ef7bf3e9da0cc2f6880060ffe0d
Lucas Abreu <lucasabreu312@gmail.com>
2022-05-10 12:26:22 -0300

    Criado um model para inserir dados no banco

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+                    new Movie { }
+
+                   
+
+                
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
