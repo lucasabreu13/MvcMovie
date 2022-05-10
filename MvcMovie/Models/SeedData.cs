5f4e12b875eaba732d99c537101a1b665f004f05
marioassis <marioassis84@hotmail.com>
2022-05-10 12:33:32 -0300

    Atualizado arquivo SeedData

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -1,8 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
-using System;
-using System.Linq;
 
 namespace MvcMovie.Models
 {
@@ -28,11 +25,27 @@ namespace MvcMovie.Models
                         Genre = "Romantic Comedy",
                         Price = 7.99M
                     },
-                    new Movie { }
-
-                   
-
-                
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
                 );
 
                 context.SaveChanges();
