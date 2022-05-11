000bec8badeeef4a422eb6b9cb5d9ff7fff4d37a
Lucas Abreu <lucasabreu312@gmail.com>
2022-05-11 11:41:11 -0300

    Atualização

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,5 +16,6 @@ namespace MvcMovie.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string? Rating { get; set; }
     }
 }
