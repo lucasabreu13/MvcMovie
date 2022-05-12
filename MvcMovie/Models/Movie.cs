7149eda420342998b894c8d786aee819065df78f
marioassis <marioassis84@hotmail.com>
2022-05-12 13:10:08 -0300

    Atualizações do projeto

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,23 +7,27 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O campo deve ter entre {2} e {1} caracteres.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Display(Name = "Titulo")]
         public string? Title { get; set; }
 
-        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [Display(Name = "Data de Lançamento")]
         public DateTime ReleaseDate { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [Required]
-        [StringLength(30)]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(30, ErrorMessage = "O tamanho máximo do campo é {1} caracteres.")]
+        [Display(Name = "Gênero")]
         public string? Genre { get; set; }
 
         [Range(1, 100)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
-    
+
+        [Display(Name = "Avaliação")]
         public string? Rating { get; set; }
     }
 }
